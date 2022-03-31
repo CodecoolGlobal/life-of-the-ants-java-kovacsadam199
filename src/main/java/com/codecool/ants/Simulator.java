75aa27008b97eada0372a55925522503549a1d7f
kovacsadam199 <kovacsadam199@gmail.com>
2022-03-31 21:37:26 +0200

    basic simulation with enter

--- src/main/java/com/codecool/ants/Simulator.java
+++ src/main/java/com/codecool/ants/Simulator.java
@@ -8,11 +8,20 @@ public class Simulator {
         Scanner input = new Scanner(System.in);
         AntColony antColony = new AntColony(9);
         antColony.display();
-        for (int i = 0; i < 10; i++) {
-                antColony.update(9, antColony.getQueenPosition());
-                antColony.display();
-
+        String enter=" ";
+        boolean run=true;
+        System.out.println("Press enter to continue");
+        while (run){
+            enter=input.nextLine();
+            if (enter.equals("quit") || enter.equals("q")){
+                run=false;
+                System.out.println("The program exits");
+                continue;}
+            antColony.update(9, antColony.getQueenPosition());
+            antColony.display();
+            System.out.println("Press enter to continue");
         }
+
 //        System.out.println("You can use `mvn package; java -jar target/ants-1.0.jar` to run this app from the console");
     }
 
